ba2acbc16927458cfc7777afa73880429d24715c
Ayaz <syedayaz225@gmail.com>
2024-02-01 13:58:02 +0100

    smart notes app

--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -20,5 +20,6 @@ from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', views.home)
+    path('home', views.home),
+    path('authorized', views.authorized)
 ]
